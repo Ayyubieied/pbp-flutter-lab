d79dd940f5dedd4f281a8cf1c74c64e90e661fdd
ayyubieied <ayyubieied@gmail.com>
2022-11-17 11:48:43 +0700

    FEAT: Revisi Tugas 8

---
+++ README_8.md
@@ -0,0 +1,5 @@
+# Tugas 8 Flutter Form
+Pemrograman Berbasis Platform (CSGE602022) - diselenggarakan oleh Fakultas Ilmu Komputer Universitas Indonesia, Semester Ganjil 2022/2023
+
+## Nama : Ied Mubaraque Sultan Salahuddine El Ayyubie
+## NPM : 2106751120
--- lib/form.dart
+++ lib/form.dart
@@ -1,233 +1,198 @@
 import 'package:flutter/material.dart';
-import 'package:counter_7/budget.dart';
-import 'package:counter_7/drawer.dart';
+import 'package:counter_7/main.dart';
+import 'package:counter_7/show.dart';
 
-class FormBudget extends StatefulWidget {
-  const FormBudget({super.key});
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
 
   @override
-  State<FormBudget> createState() => _FormBudgetState();
+  State<MyFormPage> createState() => _MyFormPageState();
 }
 
-class _FormBudgetState extends State<FormBudget> {
+class Budget {
+  int budgetNominal;
+  String budgetJudul;
+  String budgetJenis;
+  static List<Budget> budgets = [];
+  Budget(
+      String this.budgetJudul, int this.budgetNominal, String this.budgetJenis);
+}
+
+class _MyFormPageState extends State<MyFormPage> {
   final _formKey = GlobalKey<FormState>();
-  String _judul = '';
+  String _judul = "";
   int _nominal = 0;
-  String _jenis = '';
-  DateTime? _tanggal = DateTime.now();
+  String? _jenis;
+  List<String> _dropdownList = ['Pemasukan', 'Pengeluaran'];
+
+  bool _isNumeric(String? s) {
+    if (s == null) {
+      return false;
+    }
+    return double.tryParse(s) != null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Form Budget'),
+        title: Text('Form Budget'),
       ),
-      drawer: buildDrawer(context),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('counter_7'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Tambah Budget'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyFormPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Data Budget'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyShowPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Form(
         key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                // Input Judul
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Judul",
-                    labelText: "Judul",
-                    // Menambahkan circular border agar lebih rapi
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                  ),
-                  // Menambahkan behavior saat nama diketik
-                  onChanged: (String? value) {
-                    setState(() {
-                      _judul = value!;
-                    });
-                  },
-                  // Menambahkan behavior saat data disimpan
-                  onSaved: (String? value) {
-                    setState(() {
-                      _judul = value!;
-                    });
-                  },
-                  // Validator sebagai validasi form
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Judul tidak boleh kosong!';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                // Input Nominal
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Nominal",
-                    labelText: "Nominal",
-                    // Menambahkan circular border agar lebih rapi
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                  ),
-                  keyboardType: TextInputType.number,
-                  // Menambahkan behavior saat nominal diketik
-                  onChanged: (String? value) {
-                    setState(() {
-                      if (value == null || value.isEmpty) {
-                        _nominal = 0;
-                      } else {
-                        _nominal = int.parse(value);
-                      }
-                    });
-                  },
-                  // Menambahkan behavior saat data disimpan
-                  onSaved: (String? value) {
-                    setState(() {
-                      if (value == null || value.isEmpty) {
-                        _nominal = 0;
-                      } else {
-                        _nominal = int.parse(value);
-                      }
-                    });
-                  },
-                  // Validator sebagai validasi form
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Nominal tidak boleh kosong!';
-                    }
-                    if (int.parse(value) == 0) {
-                      return 'Nominal tidak boleh nol!';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                // Input Jenis
-                padding: const EdgeInsets.all(8.0),
-                child: DropdownButton<String>(
-                  value: null,
-                  icon: const Icon(Icons.keyboard_arrow_down),
-                  hint: Text(_jenis == '' ? 'Pilih jenis' : _jenis),
-                  items: const <DropdownMenuItem<String>>[
-                    DropdownMenuItem<String>(
-                      value: 'Pengeluaran',
-                      child: Text('Pengeluaran'),
-                    ),
-                    DropdownMenuItem<String>(
-                      value: 'Pemasukan',
-                      child: Text('Pemasukan'),
-                    ),
-                  ],
-                  onChanged: (String? value) {
-                    setState(() {
-                      _jenis = value!;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                // Input Jenis
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.calendar_month_outlined,
-                      color: Colors.blue,
-                    ),
-                    const SizedBox(width: 4.0),
-                    TextButton(
-                      onPressed: (() {
-                        showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2999),
-                        ).then((value) {
-                          setState(() {
-                            _tanggal = value;
-                          });
-                        });
-                      }),
-                      child: Text(
-                        _tanggal.toString().split(' ')[0],
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          // color: Colors.black87,
-                          fontWeight: FontWeight.normal,
-                        ),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Judul",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
                     ),
-                  ],
+                    // Menambahkan behavior saat nama diketik
+                    onChanged: (String? value) {
+                      setState(() {
+                        _judul = value!;
+                      });
+                    },
+                    // Menambahkan behavior saat data disimpan
+                    onSaved: (String? value) {
+                      setState(() {
+                        _judul = value!;
+                      });
+                    },
+                    // Validator sebagai validasi form
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Judul tidak boleh kosong!';
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Nominal",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    // Menambahkan behavior saat nama diketik
+                    onChanged: (String? value) {
+                      setState(() {
+                        if (_isNumeric(value)) {
+                          _nominal = int.parse(value!);
+                        }
+                      });
+                    },
+                    // Menambahkan behavior saat data disimpan
+                    onSaved: (String? value) {
+                      setState(() {
+                        if (_isNumeric(value)) {
+                          _nominal = int.parse(value!);
+                        }
+                      });
+                    },
+                    // Validator sebagai validasi form
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nominal tidak boleh kosong!';
+                      } else if (!_isNumeric(value)) {
+                        return 'Nominal harus berupa angka';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField(
+                    hint: const Text("Pilih Jenis"),
+                    icon: const Icon(Icons.keyboard_arrow_down_outlined),
+                    value: _jenis,
+                    items: _dropdownList.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _jenis = newValue!;
+                      });
+                    },
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Silahkan Pilih Jenis!';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
                   child: TextButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        Budget.add(
-                          judul: _judul,
-                          nominal: _nominal,
-                          jenis: _jenis,
-                          tanggal: _tanggal,
-                        );
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return Dialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              elevation: 15,
-                              child: ListView(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 20.0,
-                                  vertical: 16.0,
-                                ),
-                                shrinkWrap: true,
-                                children: [
-                                  Center(
-                                    child: Text(
-                                      "Berhasil menambahkan $_jenis $_judul sebesar ${_nominal.toString()}",
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('Kembali'),
-                                  )
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                        _formKey.currentState!.reset();
+                        Budget newBudget = Budget(_judul, _nominal, _jenis!);
+                        Budget.budgets.add(newBudget);
+                        _formKey.currentState?.reset();
                       }
                     },
                     style: ButtonStyle(
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(12.0)),
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue)),
                     child: const Text(
-                      "Simpan",
+                      "Submit",
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
